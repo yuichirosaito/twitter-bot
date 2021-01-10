c485640fa3bd195ab9523fe3996d3d1e68e4bbf7
yuichirosaito <l.0821.enjoy@icloud.com>
2021-01-10 17:18:58 +0900

    commit

--- amazon_book_tweet.py
+++ amazon_book_tweet.py
@@ -12,7 +12,7 @@ ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
 ACCESS_TOKEN_SECRET = os.environ['ACCESS_TOKEN_SECRET']
 
 # 以下のリンクは、先ほどコピーしておいたAmazonのURLで置き換える。ここでは「リーダブルコード」の詳細ページのURLになっている。
-AMAZON_BOOK_URL = 'https://www.amazon.co.jp/%E3%83%AA%E3%83%BC%E3%83%80%E3%83%96%E3%83%AB%E3%82%B3%E3%83%BC%E3%83%89-%E2%80%95%E3%82%88%E3%82%8A%E8%89%AF%E3%81%84%E3%82%B3%E3%83%BC%E3%83%89%E3%82%92%E6%9B%B8%E3%81%8F%E3%81%9F%E3%82%81%E3%81%AE%E3%82%B7%E3%83%B3%E3%83%97%E3%83%AB%E3%81%A7%E5%AE%9F%E8%B7%B5%E7%9A%84%E3%81%AA%E3%83%86%E3%82%AF%E3%83%8B%E3%83%83%E3%82%AF-Theory-practice-Boswell/dp/4873115655/'
+AMAZON_BOOK_URL = 'https://www.amazon.co.jp/gp/product/4414304229/ref=ox_sc_saved_title_3?smid=AN1VRQENFRJN5&psc=1'
 TEXT = 'チュートリアル実践中！ #さぼりエンジニア養成所'
 
 # いずれかのコメント文章をランダムに利用する。

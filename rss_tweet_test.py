5b352969162afa362031e98a7000310bcb2d6a5c
yuichirosaito <l.0821.enjoy@icloud.com>
2021-01-15 18:19:10 +0900

    revise

--- rss_tweet_test.py
+++ rss_tweet_test.py
@@ -5,6 +5,7 @@ import os
 import urllib
 import json
 import pprint
+from IPython import embed
 
 # 以下4つ「xxxxx」を、先ほど控えた値で書き換える。
 CONSUMER_KEY = os.environ['CONSUMER_KEY']
@@ -12,7 +13,7 @@ CONSUMER_SECRET = os.environ['CONSUMER_SECRET']
 ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
 ACCESS_TOKEN_SECRET = os.environ['ACCESS_TOKEN_SECRET']
 
-s = '東京オリンピック'
+s = '東京オリンピック 東京五輪'
 #検索ワードをURLエンコードに変換
 s_quote = urllib.parse.quote(s)
 #グーグルニュース検索のURLの間に挟む
@@ -20,20 +21,34 @@ RSS_URL = "https://news.google.com/news/rss/search/section/q/" + s_quote + "/" +
 
 # RSSのデータ取得
 rss_dict = feedparser.parse(RSS_URL)
-
-# RSSの記事データを1記事ずつ処理していく
-entry_count = 0
+news = []
 for entry in rss_dict.entries:
+    
+    p = entry.published_parsed
+    sortkey = "%04d%02d%02d%02d%02d%02d" % (p.tm_year, p.tm_mon, p.tm_mday, p.tm_hour, p.tm_min, p.tm_sec)
+
+    tmp = {
+    "title": entry.title,
+    "link": entry.link,
+    "sortkey": sortkey
+    }
+
+    news.append(tmp)
+
+new_news = sorted(news, key=lambda x:x['sortkey'], reverse=True)
+entry_count = 0
+for entry in new_news:
     entry_count += 1
     # 1記事分だけツイートしたいので、2個目以降の記事の処理が始まったら処理を抜ける。
     if entry_count > 3:
         break
 
     # ツイートする文章を作成する。
-    tweet_text = '{}\n\n{}'.format(entry.title, entry.link)
+    tweet_text = '{:.120}\n\n{}'.format(entry['title'], entry['link'])
 
     # 記事のタイトルとリンクと一言をツイートする。
     auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
     auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
     api = tweepy.API(auth)
     api.update_status(status=tweet_text)
+
